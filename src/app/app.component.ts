599b480ca64ef5d5fa9e9843b81d18a598c5f570
Quan Tran <quan.caotran2@halliburton.com>
2023-02-06 21:58:35 +0700

    add first line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}, and something, and some</div>`,
+  template: `<div>Hello {{value}}, this is the first line, and add some</div>`,
 })
 export class AppComponent {
   value = 'World';
