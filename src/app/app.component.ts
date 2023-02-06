de3cbd38bb4f00ec09bed12beaab8bb3f06c0d9f
Quan Tran <quan.caotran2@halliburton.com>
2023-02-06 21:49:56 +0700

    add some

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}, and something</div>`,
+  template: `<div>Hello {{value}}, and something, and some</div>`,
 })
 export class AppComponent {
   value = 'World';
